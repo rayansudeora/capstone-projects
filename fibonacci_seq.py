4671cfd1a6e004d62ec7b2fa8065c5da0281ef93
rayansudeora <59887243+rayansudeora@users.noreply.github.com>
2020-06-02 13:49:25 -0700

    added comments

--- fibonacci_seq.py
+++ fibonacci_seq.py
@@ -1,9 +1,9 @@
 def fib_seq():
     a=0
     b=1
-    n = int(input("The fibonacci sequence will run this amount of times: "))
-    for x in range(n):
-        a,b=b,a+b
+    n = int(input("The fibonacci sequence will run this amount of times: ")) 
+    for x in range(n): #user gives the value which dictates how many times the program will run
+        a,b=b,a+b #fibonacci sequence (the next number is the sum of the previous two.)
         yield a
     
 
--- vowels_or_consonants.py
+++ vowels_or_consonants.py
@@ -5,13 +5,13 @@ def vowels_consonants():
     count_vowels = 0
     count_consonants = 0
     for letter in word:
-        if letter.lower() in vowels:
-            count_vowels+=1
+        if letter.lower() in vowels: 
+            count_vowels+=1     #if the letter is a vowel, add 1 to the total number of vowels
         else:
             if letter.lower() in consonants:   
-                count_consonants+=1
+                count_consonants+=1     #if the letter is a consonant, add 1 to the total number of consonants
     print("Vowels: "+ str(count_vowels) + "\nConsonants: " + str(count_consonants))
     
 
-if __name__ == '__main__':
+if __name__ == '__main__': #if a user is running the program, run the function above
     vowels_consonants()
